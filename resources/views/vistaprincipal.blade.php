eca6a92552088d2d7336d002527c5821d0da3587
JMMendi <mendizabaly@gmail.com>
2025-03-07 13:11:00 +0100

    Subida de la plantilla

---
+++ resources/views/vistaprincipal.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Principal</title>
+</head>
+<body>
+    <main>
+        <h1>Página principal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate minus ut modi ullam aliquid commodi nulla soluta voluptates. Modi, officiis!</p>
+
+        <form action="">
+            <input type="text" id="nombre" name="nombre" />
+            <button>Pulse aquí</button>
+        </form>
+    </main>
+</body>
+</html>
